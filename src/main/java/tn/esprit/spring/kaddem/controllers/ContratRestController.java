6f81dd841acd530a3cc5f9766d4e00e9bdbcbf73
belguithhabib <habib.belguith@esprit.tn>
2023-10-14 13:56:38 +0100

    test push

--- src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
@@ -23,6 +23,7 @@ public class ContratRestController {
 		List<Contrat> listContrats = contratService.retrieveAllContrats();
 		return listContrats;
 	}
+
 	// http://localhost:8089/Kaddem/contrat/retrieve-contrat/8
 	@GetMapping("/retrieve-contrat/{contrat-id}")
 	public Contrat retrieveContrat(@PathVariable("contrat-id") Integer contratId) {
